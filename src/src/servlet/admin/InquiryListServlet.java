9dc5be6cff8a1fc764786a1e9919fd1711ac88b9
dojo2023-haruta <130334521+dojo2023-haruta@users.noreply.github.com>
2023-06-13 11:16:07 +0900

    管理者jspをサーブレットに移動

--- src/src/servlet/admin/InquiryListServlet.java
+++ src/src/servlet/admin/InquiryListServlet.java
@@ -1,6 +1,8 @@
 package servlet.admin;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/InquiryListServlet")
 public class InquiryListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,16 +28,16 @@ public class InquiryListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	// お問い合わせ画面にフォワードする。
+	RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/inquiry_list.jsp");
+	dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	//にポストする。
 	}
 
 }
--- src/src/servlet/admin/UsersListServlet.java
+++ src/src/servlet/admin/UsersListServlet.java
@@ -1,6 +1,8 @@
 package servlet.admin;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/UsersListServlet")
 public class UsersListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,16 +28,17 @@ public class UsersListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	// ユーザー一覧画面にフォワードする。
+	RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/users_list.jsp");
+	dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	//にポストする。
+
 	}
 
 }
