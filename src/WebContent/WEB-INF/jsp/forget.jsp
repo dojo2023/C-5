b0c7c31ed6118f230f124f71c39f8cd45195d747
dojo2023-miyake <RyuseiMiyakeplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-13 11:16:06 +0900

    リダイレクト

--- src/WebContent/WEB-INF/jsp/forget.jsp
+++ src/WebContent/WEB-INF/jsp/forget.jsp
@@ -7,7 +7,7 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>パスワード忘れ</h1>
+<h1>パスワード再登録</h1>
 
 </body>
 </html>
--- src/src/servlet/user/ForgetPasswordServlet.java
+++ src/src/servlet/user/ForgetPasswordServlet.java
@@ -1,6 +1,8 @@
 package servlet.user;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ForgetPasswordServlet")
 public class ForgetPasswordServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,8 +28,9 @@ public class ForgetPasswordServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//パスワード再登録ページにフォワード
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/forget.jsp");
+				dispatcher.forward(request, response);
 	}
 
 	/**
