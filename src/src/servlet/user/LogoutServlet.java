e25d8eab6eb7076a7ec101253e57b715c8a89bb2
dojo2023-matsumoto <130331266+dojo2023-matsumoto@users.noreply.github.com>
2023-06-13 11:16:08 +0900

    メニュー画面

--- src/src/servlet/user/LogoutServlet.java
+++ src/src/servlet/user/LogoutServlet.java
@@ -1,6 +1,7 @@
 package servlet.user;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,8 +27,8 @@ public class LogoutServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// ログインサーブレットにリダイレクトする
+		response.sendRedirect("/mecar/LoginServlet");
 	}
 
 	/**
