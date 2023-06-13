05ca7aeccef7466487cdbf0c3a6789a1d5cba845
dojo2023-hatakeyama <130334594+dojo2023-hatakeyama@users.noreply.github.com>
2023-06-13 16:53:49 +0900

    Update InquiriesDao.java

--- src/src/dao/InquiriesDao.java
+++ src/src/dao/InquiriesDao.java
@@ -1,5 +1,15 @@
 package dao;
 
-public class InquiriesDao {
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
+import model.Inquiries;
+
+public class InquiriesDao {
+	// 引数paramで検索項目を指定し、検索結果のリストを返す
+	public List<Inquiries> select(Inquiries param) {
+	Connection conn = null;
+	List<Inquiries> inqueriesList = new ArrayList<Inquiries>();
+	}
 }
