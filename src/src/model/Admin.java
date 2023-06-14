54aaa4e0fd165294b23d5f9f45dd54bfdfb7446a
dojo2023-haruta <130334521+dojo2023-haruta@users.noreply.github.com>
2023-06-14 18:00:37 +0900

    管理者編集

--- src/src/model/Admin.java
+++ src/src/model/Admin.java
@@ -1,5 +1,33 @@
 package model;
+import java.io.Serializable;
 
-public class Admin {
+public class Admin implements Serializable {
+	private String id;	// ID
+	private String pw;	// PW
 
+	public Admin(String id, String pw) {
+		this.id = id;
+		this.pw = pw;
+	}
+
+	public Admin() {
+		this.id = "";
+		this.pw = "";
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
 }
