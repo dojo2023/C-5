16bb1dd8327c4dbfb361b4fd9af84118b459d372
dojo2023-miyake <RyuseiMiyakeplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-15 15:52:56 +0900

    復元

--- src/src/servlet/user/RegistUserServlet.java
+++ src/src/servlet/user/RegistUserServlet.java
@@ -9,6 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.BcDAO;
+import model.Bc;
+import model.Result;
+
 /**
  * Servlet implementation class RegistUserServlet
  */
@@ -32,6 +36,25 @@ public class RegistUserServlet extends HttpServlet {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");
 		dispatcher.forward(request, response);
 	}
+
+	// リクエストパラメータを取得する
+			request.setCharacterEncoding("UTF-8");
+			String user_auto_id = request.getParameter("user_auto_id");
+			String user_id = request.getParameter("user_id");
+			String user_pw = request.getParameter("user_pw");
+			String user_mail = request.getParameter("user_mail");
+
+
+			// 登録処理を行う
+			UsersDAO bDao = new UsersDAO();
+			if (bDao.insert(new Bc(user_auto_id,user_id,user_pw,user_mail))) {	// 登録成功
+				request.setAttribute("result",
+				new Result("登録成功！", "レコードを登録しました。", "/mecar/RegistServlet"));
+			}
+			else {												// 登録失敗
+				request.setAttribute("result",
+				new Result("登録失敗！", "レコードを登録できませんでした。", "/mecar/RegistServlet"));
+			}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
