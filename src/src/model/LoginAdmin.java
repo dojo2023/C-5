abcbad6c7d5d3ad0aa761d688b8fca8ccba5d36d
dojo2023-haruta <130334521+dojo2023-haruta@users.noreply.github.com>
2023-06-16 11:12:06 +0900

    管理者編集

--- src/src/model/LoginAdmin.java
+++ src/src/model/LoginAdmin.java
@@ -2,6 +2,21 @@ package model;
 import java.io.Serializable;
 
 public class LoginAdmin implements Serializable {
-	private String user_id;	// ID
-	private String user_mail;	// ユーザーメール
+	private String id; //ログイン時のID
+
+	public LoginAdmin() {
+		this(null);
+	}
+
+	public LoginAdmin(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setUserId(String id) {
+		this.id = id;
+	}
 }
