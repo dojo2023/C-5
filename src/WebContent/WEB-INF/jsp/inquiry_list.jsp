b75c7462a45e429ae412dc89173538117b81eccb
dojo2023-haruta <130334521+dojo2023-haruta@users.noreply.github.com>
2023-06-19 17:12:16 +0900

    管理者編集

--- src/WebContent/WEB-INF/jsp/inquiry_list.jsp
+++ src/WebContent/WEB-INF/jsp/inquiry_list.jsp
@@ -1,11 +1,13 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
 <link rel="stylesheet" href="/mecar/css/common.css">
+<link rel="stylesheet" href="/mecar/css/inquiry_list.css">
 </head>
 <body>
 <!-- リボンメニュー -->
@@ -23,19 +25,32 @@
 		<h1>お問い合わせ一覧画面</h1>
 		<h2>お問い合わせ一覧</h2>
 		    <form id="form"method="POST" action="/mecar/InquiryListServlet">
-		     <div class="container">
-  	        <input type="text" name="ID" placeholder="検索するワード">
-  	        <input type="image" src="/mecar/img/虫眼鏡ブルー.png" alt="検索ボタン">
-  	        </div>
-            </form>
+		     <div class="right-align">
+  	         <input type="text" name="ID" placeholder="検索するワード">
+  	         <input type="image" src="/mecar/img/虫眼鏡ブルー.png" alt="検索ボタン">
+  	         </div>
+             </form>
+            	<div class="table-container">
             	<table border="1">
 					<tr>
 					<td>対応ステータス</td>
 					<td>ユーザーID</td>
 					<td>メールアドレス</td>
 					<td>件名</td>
+					<td>日付</td>
 					</tr>
-            	</table>
+
+				 <c:forEach var="e" items="${cardList2}">
+       			 <tr>
+         	     <td>${e.inquiry_status}</td>
+          		 <td>${e.user_id}</td>
+           		 <td>${e.user_mail}</td>
+          		 <td>${e.inquiry_subject}</td>
+        		 <td>${e.inquiry_date}</td>
+      			 </tr>
+   				 </c:forEach>
+              	 </table>
+            	 </div>
     </main>
 <footer>
 	<p>&copy;Copyright Error Maker. All rights reserved.</p>
--- src/src/servlet/admin/InquiryListServlet.java
+++ src/src/servlet/admin/InquiryListServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import dao.InquiriesDao;
 import model.Inquiries;
@@ -33,6 +32,16 @@ public class InquiryListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//空文字でデータを入力する。カードリスト2とか
+		InquiriesDao aInquiries = new InquiriesDao();
+
+		//クラス名	    変数名     =  代入する値;
+		List<Inquiries> cardList2 = aInquiries.select("");
+
+
+		// 検索結果をリクエストスコープに格納する。
+		request.setAttribute("cardList2" , cardList2);
+
 	// お問い合わせ画面にフォワードする。
 	RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/inquiry_list.jsp");
 	dispatcher.forward(request, response);
@@ -43,14 +52,12 @@ public class InquiryListServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// お問い合わせ検索結果画面にフォワードする
-
-
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = request.getSession();
-		if (session.getAttribute("id") == null) {
-			response.sendRedirect("/mecar/LoginAdminServlet");
-			return;
-		}
+		//HttpSession session = request.getSession();
+		//if (session.getAttribute("id") == null) {
+			//response.sendRedirect("/mecar/LoginAdminServlet");
+			//return;
+		//}
 
 		// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
