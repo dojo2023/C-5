56be8c1ceb25df88dfcef255f17ae9e2726e94e5
dojo2023-Hada <130330969+dojo2023-Hada@users.noreply.github.com>
2023-06-19 16:54:40 +0900

    6/19　羽田
    items絡み

--- src/src/servlet/user/DecreaseServlet.java
+++ src/src/servlet/user/DecreaseServlet.java
@@ -2,6 +2,7 @@ package servlet.user;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,30 +17,27 @@ public class DecreaseServlet extends HttpServlet {
 	//ここを呼ぶと、JSPにforWordする
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public DecreaseServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		//メニュー画面にフォワ―ドする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/menu.jsp");
+				dispatcher.forward(request, response);
+				}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
 
 
-		doGet(request, response);
+
+
 	}
 
 }
