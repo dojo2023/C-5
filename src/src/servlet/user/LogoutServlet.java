3afd01bd721111c5c66f864ccfab80abc4ed3b41
dojo2023-matsumoto <130331266+dojo2023-matsumoto@users.noreply.github.com>
2023-06-19 17:40:38 +0900

    松本

--- src/src/servlet/user/LogoutServlet.java
+++ src/src/servlet/user/LogoutServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LogoutServlet
@@ -15,18 +16,14 @@ import javax.servlet.http.HttpServletResponse;
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LogoutServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//セッションスコープを破棄する
+		HttpSession session = request.getSession();
+		session.invalidate();
+
 		// ログインサーブレットにリダイレクトする
 		response.sendRedirect("/mecar/LoginServlet");
 	}
--- src/src/servlet/user/SearchItemServlet.java
+++ src/src/servlet/user/SearchItemServlet.java
@@ -1,11 +1,18 @@
 package servlet.user;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.ItemsDao;
+import model.Items;
 
 /**
  * Servlet implementation class SearchItemServlet
@@ -13,14 +20,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/SearchItemServlet")
 public class SearchItemServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SearchItemServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -34,8 +33,27 @@ public class SearchItemServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		//もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user_id") == null) {
+			response.sendRedirect("/mecar/LoginServlet");
+			return;
+		}
+
+		//リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String keyWord = request.getParameter("item_name");
+
+		//検索処理を行う
+		ItemsDao iDao = new ItemsDao();
+		List<Items> cardList = iDao.select(keyWord);
+
+		//検索結果をリクエストスコープに格納する。
+		request.setAttribute("cardList", cardList);
+
+		//結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/search_result.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
