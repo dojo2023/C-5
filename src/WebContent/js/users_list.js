0e97595eb2dda34be6cbd408b5e743a7f9855aec
dojo2023-haruta <130334521+dojo2023-haruta@users.noreply.github.com>
2023-06-21 17:10:55 +0900

    管理者編集

--- src/WebContent/js/users_list.js
+++ src/WebContent/js/users_list.js
@@ -1,3 +1,27 @@
 /**
- * 
+ *
  */
+'use strict';
+
+/* 削除ボタンをクリックした時の処理 */
+
+var formObj = document.getElementById('delete_form');
+function MoveCheck(){
+event.preventDefault();
+
+
+  // 削除を押した時に送信確認のダイアログボックスを表示する
+   var res = confirm("削除しますか？");
+     if(res === true){
+        // OKなら送信してページ遷移
+       alert("削除しました。");
+       window.location.href = "/mecar/UsersListServlet";
+     }
+     else{
+        // キャンセルならアラートボックスを表示
+       alert("キャンセルしました");
+}
+}
+// 削除ボタンにイベントリスナーを追加
+var deleteButton = document.getElementById('delete_button');
+deleteButton.addEventListener('click', MoveCheck);
