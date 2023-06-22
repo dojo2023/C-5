7e264dd23097a09dfb04b96701411427fa6baa28
dojo2023-hatakeyama <130334594+dojo2023-hatakeyama@users.noreply.github.com>
2023-06-22 10:37:59 +0900

    link

--- src/src/servlet/user/LinkServlet.java
+++ src/src/servlet/user/LinkServlet.java
@@ -2,15 +2,15 @@ package servlet.user;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.ItemsDao;
-import model.Items;
+import model.LoginUsers;
 
 /**
  * Servlet implementation class LinkServlet
@@ -43,13 +43,30 @@ public class LinkServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 
-		// 残量を100に戻す(ための処理をItemsDaoから呼び出す)
+		//もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user_id") == null) {
+			response.sendRedirect("/mecar/LoginServlet");
+			return;
+		}
+
+		//リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("name");  // 商品名
+		String url = request.getParameter("url");  // 商品URL
+
+		// セッションスコープからインスタンスを取得する（商品IDを取得する準備）
+		LoginUsers user_id = (LoginUsers) session.getAttribute("user_id");
+
+		//ItemsDaoのメソッドを呼び出し、item_idを取得する
+		ItemsDao iDao = new ItemsDao();
+		int item_id = iDao.getItemId((String)user_id.getUser_id(), name, url);
+
+		// 残量を＋100する(ための処理をItemsDaoから呼び出す)
 		ItemsDao bDao = new ItemsDao();
-		bDao.update(new Items());
+		bDao.updateMater(item_id);
 
-		// リンク先にフォワード（jspに飛ぶ）する
-		RequestDispatcher dispatcher = request.getRequestDispatcher("");
-		dispatcher.forward(request, response);
+		// リンク先に飛ぶ(jsp)
+
 	}
-
 }
